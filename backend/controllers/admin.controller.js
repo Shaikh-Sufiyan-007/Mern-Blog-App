d6e3da39d0c6d1f5d2b68eeeae360ca163c8c421
Shaikh-Sufiyan-007 <sufiyanali1705@gmail.com>
2026-02-08 19:59:01 +0530

    2 - making login route for admin

---
+++ backend/controllers/admin.controller.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken'
+
+export const adminLogin = async(req, res) => {
+    try {
+        const {email, password} = req.body
+        if(!email || !password) return res.status(400).json({message: "Please enter email and password"});
+
+        if(email !== process.env.ADMIN_EMAIL) {
+            return res.status(400).json({message: "Invalid email"});
+        }
+
+        if(password !== process.env.ADMIN_PASSWORD) {
+            return res.status(400).json({message: "Invalid password"});
+        }
+
+        const token = jwt.sign({ email }, process.env.JWT_SECRET, {expiresIn: "1d"});
+        res.cookie("token", token, {httpOnly: true});
+
+        res.status(200).json({success: true ,message: "Login successful", token});
+    } catch (error) {
+        res.status(500).json({success: false,message: error.message});
+        console.log(error);
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import { connectDB } from './config/db.js';
+import adminRouter from './routes/admin.route.js';
 
 const app = express()
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/admin', adminRouter);
 
 connectDB();
 const PORT = process.env.PORT || 5000;
